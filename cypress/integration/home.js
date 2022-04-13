530590195a534444bb520f10f94bbf23fcc13e50
MB <mikebeirne@gmail.com>
2022-04-13 07:36:54 -0500

    Updates to fix Fake Resto Tests

--- cypress/integration/home.js
+++ cypress/integration/home.js
@@ -1,21 +1,21 @@
 describe('On the home page', () => {
 
     it('Can load the page', () => {
-      cy.visit('/test.html')
-      cy.title().should('eq', 'QA Web Automation Activity')
+      cy.visit('/qa-fake-restaurant')
+      cy.title().should('eq', 'Our Fake Restaurant')
     })
   
     it('Can view a menu item', () => {
-      cy.visit('/test.html')
+      cy.visit('/qa-fake-restaurant')
   
       // Help! This worked before, but then the page changed, and now it's failing!
-      cy.get('html body div div button').click()
+      cy.get('#button_container > .w3-button').click()
   
       cy.get('h5').first().should('be.visible').should('contain', 'Tomato Soup')
     })
   
     it('Can enter contact details', () => {
-      cy.visit('/test.html')
+      cy.visit('/qa-fake-restaurant')
   
       cy.contains('div', 'show contact').click()
       cy.contains('contact').should('be.visible')
@@ -29,12 +29,14 @@ describe('On the home page', () => {
     })
   
     it('Can toggle the contact form', () => {
-      cy.visit('/test.html')
+      cy.visit('/qa-fake-restaurant')
   
       cy.contains('div', 'show contact').click()
       cy.contains('Reserve a table').should('be.visible')
   
-      cy.contains('div', 'hide contact').click()
+      cy.get('#_541jkl_modal > .w3-modal-content > .w3-black > .w3-button').click()
+      // cy.contains('div', 'hide contact').click()
+      // NOTE TO QA: Let's check with product owner to confirm what functionality should be for the `show/hide contact` button
       cy.contains('Reserve a table').should('not.be.visible')
     })
   })
